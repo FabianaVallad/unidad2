4e3bfdeb0e186381f8da7039d6f9362374da35b7
Fabianavf239 <fabianavf239@gmail.com>
2024-08-12 19:37:15 -0500

    s29

---
+++ SEMANA6/S29/assets/js/aritméticas.js
@@ -0,0 +1,7 @@
+//FUNCIÓN PARA SUMAR DOS NÚMEROS
+function sumar (a, b) {
+    return a + b;
+}
+
+//EXPORTACIONES
+export { sumar };
---
+++ SEMANA6/S29/assets/js/script.js
@@ -0,0 +1,4 @@
+import { sumar } from './aritméticas.js'
+
+//USAMOS LA FUNCIÓN SUMAR
+console.log(sumar(15, 34));
